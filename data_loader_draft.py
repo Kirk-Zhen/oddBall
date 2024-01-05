983b34e8ad11f6cddf4dbd0aa5f6fde39a69b437
Zijian Zhen <48104232+Kirk-Zhen@users.noreply.github.com>
2024-01-05 17:10:47 +0800

    update

--- data_loader_draft.py
+++ data_loader_draft.py
@@ -13,6 +13,44 @@ def load_csv_data(path, directed:bool=False):
     return G
 
 
-path = 'data/enron_cnt.csv'
-G =  load_csv_data(path, directed=True)
+# path = 'data/enron_cnt.csv'
+path = 'data/sample.csv'
+G =  load_csv_data(path, directed=False)
 
+print(G.nodes())
+
+'''
+feature dictionary which format is { node i's id : [Ni, Ei, Wi, λw,i] }
+Ni:     number of neighbors (degress) of ego i
+Ei:     number of edges in egonet i
+Wi:     total weight of egonet i
+λw,i:   the principal eigenvalue(the maximum eigenvalue with abs) of egonet i's weighted adjacency matrix
+'''
+featureDict = {}
+nodes = list(G.nodes)
+# iterate all nodes in the graph 
+featureDict = {}
+nodelist = list(G.nodes)
+for node in nodelist[2:3]:
+    featureDict[node] = []
+    #the number of node i's neighbor
+    Ni = G.degree(node)
+
+    egonet = nx.ego_graph(G, node)
+    Wi = sum([egonet[u][v]['weight'] for u,v in egonet.edges()])
+    Ei = len(egonet.edges())
+
+    egonet_adjacency_matrix = nx.adjacency_matrix(egonet).todense()
+    eigenvalue, eigenvector = np.linalg.eig(egonet_adjacency_matrix)
+    eigenvalue.sort()
+
+    # In case that the matrix is not positive definite, we need to consider the negative lambda
+    # Positive Definite: 
+    #   M is positive definite if z*Mz is positive for every nonzero complex column vector z
+    #   where z* is the conjugate transpose of z
+    Lambda_w_i = max(abs(eigenvalue[0]), abs(eigenvalue[-1]))
+    featureDict[node].append(Ni)
+    featureDict[node].append(Ei)
+    featureDict[node].append(Wi)
+    featureDict[node].append(Lambda_w_i)
+print(featureDict)
--- undirected/dataloader.py
+++ undirected/dataloader.py
@@ -36,25 +36,11 @@ def get_feature(G):
         featureDict[node] = []
         #the number of node i's neighbor
         Ni = G.degree(node)
-        featureDict[node].append(Ni)
 
-        # initialization of features:
-        Ei, Wi, Lambda_w_i = 0, 0, 0
-        Ei += Ni
+        egonet = nx.ego_graph(G, node)
+        Wi = sum([egonet[u][v]['weight'] for u,v in egonet.edges()])
+        Ei = len(egonet.edges())
 
-        iNeighbor = list(G.neighbors(node)) # the set of node i's neighbor
-        egonet = nx.Graph()
-        for nei in iNeighbor:
-            Wi += G[nei][node]['weight']
-            egonet.add_edge(node, nei, weight=G[nei][node]['weight'])
-        iNeighborLen = len(iNeighbor) # number of i's neighbor
-        for it1 in range(iNeighborLen):
-            for it2 in range(it1+1, iNeighborLen):
-                #if it1 in it2's neighbor list
-                if iNeighbor[it1] in list(G.neighbors(iNeighbor[it2])):
-                    Ei += 1
-                    Wi += G[iNeighbor[it1]][iNeighbor[it2]]['weight']
-                    egonet.add_edge(iNeighbor[it1], iNeighbor[it2], weight=G[iNeighbor[it1]][iNeighbor[it2]]['weight'])
         egonet_adjacency_matrix = nx.adjacency_matrix(egonet).todense()
         eigenvalue, eigenvector = np.linalg.eig(egonet_adjacency_matrix)
         eigenvalue.sort()
@@ -64,6 +50,7 @@ def get_feature(G):
         #   M is positive definite if z*Mz is positive for every nonzero complex column vector z
         #   where z* is the conjugate transpose of z
         Lambda_w_i = max(abs(eigenvalue[0]), abs(eigenvalue[-1]))
+        featureDict[node].append(Ni)
         featureDict[node].append(Ei)
         featureDict[node].append(Wi)
         featureDict[node].append(Lambda_w_i)
