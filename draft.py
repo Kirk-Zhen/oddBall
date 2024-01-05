ca03d8dee050b9ebb0e19078331f482ff1a289e1
Zijian Zhen <48104232+Kirk-Zhen@users.noreply.github.com>
2024-01-05 17:22:08 +0800

    update

--- draft.py
+++ draft.py
@@ -32,20 +32,19 @@ def get_feature(G):
     Wi:     total weight of egonet i
     λw,i:   the principal eigenvalue(the maximum eigenvalue with abs) of egonet i's weighted adjacency matrix
     '''
-    featureDict = {}
-    nodes = list(G.nodes)
     # iterate all nodes in the graph 
     featureDict = {}
     nodelist = list(G.nodes)
     for node in nodelist:
-        featureDict[node] = []
         #the number of node i's neighbor
         Ni = G.degree(node)
 
+        # build egonet
         egonet = nx.ego_graph(G, node)
         Wi = sum([egonet[u][v]['weight'] for u,v in egonet.edges()])
         Ei = len(egonet.edges())
 
+        # build adjacency matrix
         egonet_adjacency_matrix = nx.adjacency_matrix(egonet).todense()
         eigenvalue, eigenvector = np.linalg.eig(egonet_adjacency_matrix)
         eigenvalue.sort()
@@ -55,11 +54,9 @@ def get_feature(G):
         #   M is positive definite if z*Mz is positive for every nonzero complex column vector z
         #   where z* is the conjugate transpose of z
         Lambda_w_i = max(abs(eigenvalue[0]), abs(eigenvalue[-1]))
-        featureDict[node].append(Ni)
-        featureDict[node].append(Ei)
-        featureDict[node].append(Wi)
-        featureDict[node].append(Lambda_w_i)
+        featureDict[node] = [Ni, Ei, Wi, Lambda_w_i]
     return featureDict
+
 
 
 
