e5f2d0d332e4379c97b4bdb20239b1abe7271804
Zijian Zhen <48104232+Kirk-Zhen@users.noreply.github.com>
2024-01-19 11:06:21 +0800

    update

--- undirected/model.py
+++ undirected/model.py
@@ -40,6 +40,7 @@ def star_or_clique(featureDict):
     return outlineScoreDict
 
 
+
 # Observation 2: EWPL
 def heavy_vicinity(featureDict):
     W = [featureDict[node][2] for node in featureDict.keys()]
